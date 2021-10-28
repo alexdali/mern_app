a7fc9984308df4ebd4f473c78fed41a808f36c62
alexdali <alexdali@gmail.com>
2021-10-28 12:42:16 +0400

    feat: update auth.routes.js, add jwt authorization

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,31 +1,85 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
+const config = require('config');
+
 const router = Router();
 
 // /api/auth/register
-router.post('/register', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post(
+  '/register',
+  [
+    check('email', 'Email is incorrect').isEmail(),
+    check('password', 'The password must be at least 6 characters long').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    const candidate = await User.findOne({ email });
-    if (candidate) {
-      return res.status(400).json({ message: 'User with this email already exists' });
-    }
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'The entered data is incorrect for registration, please try again'
+        });
+      }
+      const { email, password } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new User({ email, password: hashedPassword });
-    await user.save();
-    res.status(201).json({message: 'New user created'})
+      const candidate = await User.findOne({ email });
+      if (candidate) {
+        return res.status(400).json({ message: 'User with this email already exists' });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+      await user.save();
+      res.status(201).json({message: 'New user created'})
 
   } catch (e) {
-    res.status(500).json({ message: 'Something went wrong, please try again' });
+      res.status(500).json({ message: 'Something went wrong, please try again' });
   }
 });
 
 // /api/auth/login
-router.post('login', async (req, res) => {
+router.post(
+  'login',
+  [
+    check('email', 'Enter correct email').normalizeEmail().isEmail(),
+    check('password', 'Enter password').exists(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'The entered data is incorrect for enter, please try again'
+        });
+      }
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: 'User not found' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: 'The entered password is incorrect for enter, please try again' });
+      }
+
+      const token = jwt.sign(
+        { userId: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: '1h' }
+      );
+
+      res.json({ token, userId: user.id });
+
+    } catch (e) {
+      res.status(500).json({ message: 'Something went wrong, please try again' });
+    }
 
 });
 
